e76c48ea675e95fca51fd2c1cf638340fd2b755e
Török Ádám <toroka22@ADY-NAGYATAD.SULINET.HU>
2023-09-20 08:46:41 +0200

    asd

---
+++ Valtozok.py
@@ -0,0 +1,28 @@
+#Változók próbálgatása
+
+
+szam=42
+
+print (szam)
+
+valosSzam=2.3
+
+print (valosSzam)
+
+szoveg="asdasda"
+print (szoveg)
+
+sutANap=True
+print(sutANap)
+
+szam=szam+2
+print(szam)
+szam=szam+2
+print(szam)
+
+print(szam-10)
+print(szam/10)
+
+
+szam=12
+print(szam//5)
---
+++ szamok.py
@@ -0,0 +1,8 @@
+#elso tíz pozitív egész szám 10 oszlopban!
+
+for i in range(1,101):
+    print(i, end="  ")
+
+    if i % 10 == 0:
+        print()
+    
