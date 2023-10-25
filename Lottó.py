7a0fc7feed3aa2d20ff67e7cd6cd2e475e134ff9
Török Ádám <toroka22@ADY-NAGYATAD.SULINET.HU>
2023-10-25 08:46:51 +0200

    asd

--- Lottó.py
+++ Lottó.py
@@ -1,44 +1,62 @@
 import random
 
+bekert = ""
+while bekert != 0:
+    valaszto=["Ötöslottó", "Hatoslottó", "Skandinávlottó","Totó"]
+    for i,elem in enumerate(valaszto):
+        print(i+1,":", elem)
+    print("0 : Kilépés")
+    print("#"*40)
+    bekert = int(input("Adj meg egy számota fentiek közül:"))
+    print("#"*40)
+    if bekert == 1:
+
 #otoslotto
-otosLista=[]
+        otosLista=[]
 
-while len(otosLista)<= 4:
-    randomSzam = random.randint(1,90)
-    if randomSzam not in otosLista:
-        otosLista.append(randomSzam)
+        while len(otosLista)<= 4:
+            randomSzam = random.randint(1,90)
+            if randomSzam not in otosLista:
+                otosLista.append(randomSzam)
 
-print (otosLista)
+            print("A gép áltál sorsolt Ötöslottó számok:",otosLista)
+            print("#"*40)
+    elif bekert == 2:
 
 #hatoslotto
-hatosLista=[]
+        hatosLista=[]
 
-while len(hatosLista)<= 5:
-    randomSzam = random.randint(1,45)
-    if randomSzam not in hatosLista:
-        hatosLista.append(randomSzam)
+        while len(hatosLista)<= 5:
+            randomSzam = random.randint(1,45)
+            if randomSzam not in hatosLista:
+                hatosLista.append(randomSzam)
 
-print (hatosLista)
+            print("A gép által sorsolt Hatoslottó számok:",hatosLista)
+            print("#"*40)
+    elif bekert == 3:
 
 #skandinavlotto
 
-skandinavLista=[]
+        skandinavLista=[]
 
-while len(skandinavLista)<= 6:
-    randomSzam = random.randint(1,35)
-    if randomSzam not in skandinavLista:
-        skandinavLista.append(randomSzam)
+        while len(skandinavLista)<= 6:
+            randomSzam = random.randint(1,35)
+            if randomSzam not in skandinavLista:
+                skandinavLista.append(randomSzam)
 
-print (skandinavLista)
+            print("A gép által sorsolt Skandináv lottó számok:",skandinavLista)
+            print("#"*40)
+    elif bekert == 4:
 
 
 #toto
+        toto=["1","2","x"]
+        toto2=[]
 
-toto=["1","2","x"]
+        while len(toto2)<= 13:
+            index = random.randint(0,2)
+            toto2.append(toto[index])
 
-while len(skandinavLista)<= 13:
-    randomSzam = random.randint(1,12)
-    if randomSzam not in skandinavLista:
-        skandinavLista.append(randomSzam)
-
-print (skandinavLista)
+            print (toto2)
+    elif bekert == 0:
+        break
