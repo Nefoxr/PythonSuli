7b8294f2a53bebe02aba78473175cae9baae3ef5
Török Ádám <toroka22@ADY-NAGYATAD.SULINET.HU>
2024-01-30 12:47:05 +0100

    feka

--- nevRajz.py
+++ nevRajz.py
@@ -5,63 +5,137 @@ from tkinter import *
 win=Tk()
 
 #ablak mérete
-win.geometry("600x600")
+win.geometry("1000x1000")
 
 #canvas létrehozás
 canvas=Canvas(win, width=600, height=300)
 canvas.configure(bg="lightgray")
-
 #canvas akkora amekkora az ablak
 canvas.pack(fill = BOTH, expand = 1)
-fenyo2=[100,0,
-        0,400,
-        100,400,
-        125,300,
-        200,300,
-        225,400,
-        325,400,
-        225,0,
-        100,0
-        ]
 
-adam=[
-        [100,0,
-        0,400,
-        100,400,
-        125,300,
-        200,300,
-        225,400,
-        325,400,
-        225,0,
-        100,0
-        ],
+fenyo2=[200,0,
+        0,100,
+        150,100,
+        0,200,
+        150,200,
+        0,300,
+        150,300,
+        150,400,
+        250,400,
+        250,300,
+        400,300,
+        250,200,
+        400,200,
+        250,100,
+        400,100,
+        200,0]
 
-        [120,225,
-        200,225,
-        165,62.5,
-        120,225,
-        ]]
-
-adam2=[]
-for e in adam:
-        e=transzformaciok.nagyit(e, 2)
-        e=transzformaciok.eltol(e, 100, 100)
-        e=transzformaciok.forgat(e,90)
-        
-        adam2.append(e)
-
-adam2=transzformaciok.forgat(adam2,0)
-
-print(adam2)
-
-for e in adam2:
-        canvas.create_line(e,width=5,fill="black")
-
-fenyo2Masolat=transzformaciok.eltol(fenyo2,100,100)
+fenyo2Masolat=transzformaciok.eltolas(fenyo2,100,100)
 #canvas.create_line(fenyo2Masolat,width=5,fill="green")
-#fenyo3Masolat=transzformaciok.eltol(fenyo3,100,100)
-#canvas.create_line(fenyo3Masolat,width=5,fill="green")
-#fenyo4Masolat=transzformaciok.eltol(fenyo4,100,100)
-#canvas.create_line(fenyo4Masolat,width=5,fill="green")
 
+mate=[[0,0,
+   0,100,
+   100,100,
+   100,500,
+   0,500,
+   0,600,
+   300,600,
+   300,500,
+   200,500,
+   200,100,
+   400,300,
+   600,100,
+   600,500,
+   500,500,
+   500,600,
+   800,600,
+   800,500,
+   700,500,
+   700,100,
+   800,100,
+   800,0,
+   500,0,
+   400,200,
+   300,0,
+   0,0],
+
+   ]
+a=[[0,400,
+   180,400,
+   250,250,
+   350,250,
+   420,400,
+   600,400,
+   400,0,
+   200,0,
+   0,400],
+
+   [250,200,
+    340,200,
+    370,50,
+    280,50,
+    250,200]]
+
+t=[0,0,
+   0,50,
+   175,50,
+   175,300,
+   225,300,
+   225,50,
+   400,50,
+   400,0,
+   0,0]
+
+e=[0,0,
+   0,500,
+   400,500,
+   400,400,
+   100,400,
+   100,300,
+   400,300,
+   400,200,
+   100,200,
+   100,100,
+   400,100,
+   400,0,
+   0,0
+   ]
+
+hatter="#ffffff"
+betuszinek=["red", hatter, "blue"]
+
+mate2=[]                                                                                                                                                                                                        
+#for e in mate:
+        #e=transzformaciok.nagyit(e,0.4)
+        #e=transzformaciok.eltolas(e, 100, 100)
+        #e=transzformaciok.forgat(e, 45)
+        #mate2.append(e)
+
+#mate2=transzformaciok.masol(mate)
+#mate2=transzformaciok.nagyit(mate2,0.4)
+#mate2=transzformaciok.eltolas(mate2,100,100)
+
+mate=transzformaciok.nagyit(mate,0.4)
+mate=transzformaciok.eltolas(mate,100,100)
+canvas.create_line(mate,width=5,fill="red")
+
+a=transzformaciok.nagyit(a,0.6)
+a=transzformaciok.eltolas(a,450,100)
+canvas.create_line(a,width=5,fill="pink")
+
+t=transzformaciok.nagyit(t,0.8)
+t=transzformaciok.eltolas(t,750,100)
+canvas.create_line(t,width=5,fill="purple")
+
+e=transzformaciok.nagyit(e,0.5)
+e=transzformaciok.eltolas(e,1100,100)
+canvas.create_line(e,width=5,fill="magenta")
+
+#while True:
+        #canvas.delete("all")
+        #ate2=transzformaciok.forgat(mate2,0.009)
+        #for i,e in enumerate(mate2):
+                #canvas.create_polygon(e,width=betuszinek[i],fill="blue")
+        #win.update_idletasks()
+        #win.update()
 win.mainloop()
--- transzformaciok.py
+++ transzformaciok.py
@@ -1,87 +1,99 @@
 import math
 
-#eltolás
-def eltol(pontok,x,y):
-    
-    if isinstance(pontok[0], list):
-        for i in range(len(pontok)):
-            pontok[i]=eltol(pontok[i],x,y)
-    else:
-        for i in range(0,len(pontok),2):
-            pontok[i]+=x
-            pontok[i+1]+=y
-    return pontok
+def eltolas(pontok,x,y):
+	if isinstance(pontok[0], list):
+		for i in range(len(pontok)):
+			pontok[i]=eltolas(pontok[i],x,y)
+	else:
+		for i in range(0,len(pontok),2):
+			pontok[i]+=x
+			pontok[i+1]+=y
+	return pontok
 
-#Nagyítás
 def nagyit(pontok,x,y=-1):
-    
-    if isinstance(pontok[0], list):
-        for i in range(len(pontok)):
-            pontok[i]=nagyit(pontok[i],x,y)
-    else:
-        if y==-1:
-            for i in range(len(pontok)):
-                pontok[i]*=x
-        else:
-            for i in range(len(pontok)):
-                if i%2==0:
-                    pontok[i]*=x
-                else:
-                    pontok[i]*=y
-    return pontok
+	if isinstance(pontok[0], list):
+		for i in range(len(pontok)):
+			pontok[i]=nagyit(pontok[i],x,y)
 
-#Elforgatás
+	else:
+		if y==-1:
+			#print(y)
+			for i in range(len(pontok)):
+				pontok[i]*=x
+		else:
+			for i in range(len(pontok)):
+				if i%2==0:
+					pontok[i]*=x
+				else:
+					pontok[i]*=y
+
+	return pontok
+
+#x′ = cos αx − sin αy
+#y′ = sin αx + cos αy
 def forgatPont(x,y,szog):
-    x2= math.cos(math.radians(szog))*x - math.sin(math.radians(szog))*y
-    y2=math.cos(math.radians(szog))*y + math.sin(math.radians(szog))*x
+	x2=math.cos(math.radians(szog))*x - math.sin(math.radians(szog))*y
+	y2=math.sin(math.radians(szog))*x + math.cos(math.radians(szog))*y
 
-    return x2,y2
+	return x2,y2
 
+#[ [első],[második] ]
 def forgat(lista,szog,oX="",oY=""):
-    if oX=="" and oY=="":
-        oX,oY=kozeppont(lista)
+	#a forgatás középpontjának kiszámítása, minden vonal esetén
+	if oX=="" and oY=="":
+		oX,oY=kozepSzamol(lista)
+	elif oX=="" or oY=="":
+		return lista
+		
+	if isinstance(lista[0], list):
+		for i in range(len(lista)):
+			lista[i]=forgat(lista[i],szog,oX,oY)
 
-    elif oX=="" or oY=="":
-            return lista
-    
-    if isinstance(lista[0], list):
-        for i in range(len(lista)):
-            lista[i]=forgat(lista[i],szog,oX,oY)
-    else:
+	else:
+		
+		lista=eltolas(lista,-oX,-oY)
 
-        
-        lista=eltol(lista,-oX,-oY)
-        for i in range(0,len(lista),2):
-            lista[i],lista[i+1]=forgatPont(lista[i],lista[i+1],szog)
-        lista=eltol(lista,oX,oY)
-    return lista
+		for i in range(0,len(lista),2):
+			lista[i],lista[i+1]=forgatPont(lista[i],lista[i+1],szog)
 
+		lista=eltolas(lista,oX,oY)
 
-#Középont kiszámolása; a koordináták átlag
+	return lista
 
-def kozeppont(lista):
-    
-    if isinstance(lista[0], list):
-        uj=[]
-        for i in range(len(lista)):
-            x,y=kozeppont(lista[i])
-            uj.append(x)
-            uj.append(y)
-        x,y=kozeppont(uj)
-    else:
-        xOssz=0
-        yOssz=0
-        for i in range(len(lista)):
-            print(i)
-            if i%2==0:
-                xOssz+=lista[i]
-            else:
-                yOssz+=lista[i]
-        x=xOssz/(len(lista)/2)
-        y=yOssz/(len(lista)/2)
-    return (x,y)
+def kozepSzamol(lista):
+	if isinstance(lista[0], list):
+		uj=[]
+		for i in range(len(lista)):
+			x,y=kozepSzamol(lista[i])
+			uj.append(x)
+			uj.append(y)
+		
+		x,y=kozepSzamol(uj)
+
+	else:
+		x=0
+		y=0
+		for i in range(len(lista)):
+			if i%2==0:
+				x+=lista[i]
+			else:
+				y+=lista[i]
+
+		x=x/(len(lista)/2)
+		y=y/len(lista)*2
+
+	return x,y
+
+#lista másolása, deepcopy
+def masol(lista):
+	uj=[]
+	if isinstance(lista[0],list):
+		uj=[masol(belsoLista) for belsoLista in lista]
+	else:
+		uj=[belsoErtek for belsoErtek in lista]
+	return uj
 
 if __name__ == '__main__':
-    print("Rendesen elindítva")
-else:
-    print("Modulként betöltve")
+	print ("rendesen elindítva")
+#else:
+#	print("Modulként betöltve")
