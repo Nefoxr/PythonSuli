5372d1815fd8a5409c7e6d00556250a51df2af5b
Török Ádám <toroka22@ADY-NAGYATAD.SULINET.HU>
2024-02-02 13:14:58 +0100

    balls

--- nevRajz.py
+++ nevRajz.py
@@ -5,7 +5,7 @@ from tkinter import *
 win=Tk()
 
 #ablak mérete
-win.geometry("1000x1000")
+win.geometry("2000x2000")
 
 #canvas létrehozás
 canvas=Canvas(win, width=600, height=300)
@@ -74,7 +74,8 @@ a=[[0,400,
     340,200,
     370,50,
     280,50,
-    250,200]]
+    250,200],
+    ]
 
 t=[0,0,
    0,50,
@@ -102,9 +103,12 @@ e=[0,0,
    ]
 
 hatter="#ffffff"
-betuszinek=["red", hatter, "blue"]
+betuszinek=["black", hatter, "blue"]
 
-mate2=[]                                                                                                                                                                                                        
+mate2=[[100.0, 100.0, 100.0, 140.0, 140.0, 140.0, 140.0, 300.0, 100.0, 300.0, 100.0, 340.0, 220.0, 340.0, 220.0, 300.0, 180.0, 300.0, 180.0, 140.0, 260.0, 220.0, 340.0, 140.0, 340.0, 300.0, 300.0, 300.0, 300.0, 340.0, 420.0, 340.0, 420.0, 300.0, 380.0, 300.0, 380.0, 140.0, 420.0, 140.0, 420.0, 100.0, 300.0, 100.0, 260.0, 180.0, 220.0, 100.0, 100.0, 100.0],
+[450.0, 340.0, 558.0, 340.0, 600.0, 250.0, 660.0, 250.0, 702.0, 340.0, 810.0, 340.0, 690.0, 100.0, 570.0, 100.0, 450.0, 340.0], [600.0, 220.0, 654.0, 220.0, 672.0, 130.0, 618.0, 130.0, 600.0, 220.0],
+[750.0, 100.0, 750.0, 140.0, 890.0, 140.0, 890.0, 340.0, 930.0, 340.0, 930.0, 140.0, 1070.0, 140.0, 1070.0, 100.0, 750.0, 100.0],
+[1100.0, 100.0, 1100.0, 350.0, 1300.0, 350.0, 1300.0, 300.0, 1150.0, 300.0, 1150.0, 250.0, 1300.0, 250.0, 1300.0, 200.0, 1150.0, 200.0, 1150.0, 150.0, 1300.0, 150.0, 1300.0, 100.0, 1100.0, 100.0]]                                                                                                                                                                                                        
 #for e in mate:
         #e=transzformaciok.nagyit(e,0.4)
         #e=transzformaciok.eltolas(e, 100, 100)
@@ -115,27 +119,46 @@ mate2=[]
 #mate2=transzformaciok.nagyit(mate2,0.4)
 #mate2=transzformaciok.eltolas(mate2,100,100)
 
-mate=transzformaciok.nagyit(mate,0.4)
-mate=transzformaciok.eltolas(mate,100,100)
-canvas.create_line(mate,width=5,fill="red")
 
-a=transzformaciok.nagyit(a,0.6)
-a=transzformaciok.eltolas(a,450,100)
-canvas.create_line(a,width=5,fill="pink")
+#for e in mate2:
+        #canvas.create_line(e,width=5,fill="black")
 
-t=transzformaciok.nagyit(t,0.8)
-t=transzformaciok.eltolas(t,750,100)
-canvas.create_line(t,width=5,fill="purple")
+xMozgas=0.2
+yMozgas=0.2
+while True:
+        canvas.delete("all")
+        mate2=transzformaciok.forgat(mate2,0.02)
+        magassag=win.winfo_height()
+        szelesseg=win.winfo_width()
+        print(magassag)
+        print(szelesseg)
+        xLista=[]
+        yLista=[]
+        #koordináták kigyűjtése
+        for elem in mate2:
+                for i in range(len(elem)):
+                        #X koord kigyűjtése
+                        if i%2==0:
+                                xLista.append(elem[i])
+                        #Y koord kigyűjtése
+                        else:
+                                yLista.append(elem[i])
+        #Legnagyobb koordináta megkeresése
+        xLegnagyobb=max(xLista)
+        yLegnagyobb=max(yLista)
+        #Legkisebb koordináta megkeresése
+        xLegkisebb=min(xLista)
+        yLegkisebb=min(yLista)
 
-e=transzformaciok.nagyit(e,0.5)
-e=transzformaciok.eltolas(e,1100,100)
-canvas.create_line(e,width=5,fill="magenta")
-
-#while True:
-        #canvas.delete("all")
-        #ate2=transzformaciok.forgat(mate2,0.009)
-        #for i,e in enumerate(mate2):
-                #canvas.create_polygon(e,width=betuszinek[i],fill="blue")
-        #win.update_idletasks()
-        #win.update()
+        #Változtasson irányt, ha a széléhez ér
+        if yLegnagyobb>magassag or yLegkisebb<0:
+                yMozgas*=-1
+        elif xLegnagyobb>szelesseg or xLegkisebb<0:
+                xMozgas*=-1
+        
+        mate2=transzformaciok.eltolas(mate2,xMozgas,yMozgas)
+        for i,e in enumerate(mate2):
+                canvas.create_line(e,width=5)
+        win.update_idletasks()
+        win.update()
 win.mainloop()
