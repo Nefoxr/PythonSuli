cd044ff6515b129bbe0e101619f46a2dc1ff0b8f
Török Ádám <toroka22@ADY-NAGYATAD.SULINET.HU>
2024-02-02 13:15:08 +0100

    balls

---
+++ egyszerű pong játék.py
@@ -0,0 +1,70 @@
+#egyszerű pong játék
+#Start: 2024.01.31
+#Török Ádám
+
+from tkinter import *
+import random
+
+#def randomColor():
+    #r=random.randint(0,255)
+    #g=random.randint(0,255)
+    #b=random.randint(0,255)
+    #return ("#"+hex(r)[-2:]+hex(g)[-2:]+hex(b)[-2:]).replace("x","0")
+
+def atmenetColor(red, green, blue):
+    red+=5
+    if red > 255:
+        red=red-255
+        green+=5
+    if green>255:
+        green=green-255
+        green+=5
+    if blue>255:
+        blue=blue-255
+        blue+=5
+    
+    return ("#"+hex(red)[-2:]+hex(green)[-2:]+hex(blue)[-2:]).replace("x","0"),red,green,blue   
+jatekHatter="lightgray"
+
+#print(randomColor()) 
+
+win=Tk()
+
+win.geometry("600x600+100+20")
+win.title("Pong Játék 10.B/1 2024")
+canvas=Canvas(win,bg=jatekHatter)
+canvas.pack(fill = BOTH, expand=1)
+
+
+canvas.create_oval(0,   0,100,100,fill="red")
+
+labdaSpeed=[1,1]
+labdaPos=[0,0]
+labdaSize=50
+
+labdaColor="red"
+red,green,blue=0,0,0
+
+
+while True:
+    labdaColor,red,green,blue=atmenetColor(red, green, blue)
+    labdaPos[0]+=labdaSpeed[0]
+    labdaPos[1]+=labdaSpeed[1]
+
+    if labdaPos[0]>win.winfo_width() or labdaPos[0]<0:
+        labdaSpeed[0]*=-1
+        #labdaColor=randomColor()
+
+    if labdaPos[1]>win.winfo_height() or labdaPos[1]<0:
+        labdaSpeed[1]*=-1
+        #labdaColor=randomColor()
+
+    canvas.create_oval(labdaPos[0],
+                       labdaPos[1], 
+                       labdaPos[0]+labdaSize,
+                       labdaPos[1]+labdaSize,
+                        fill=labdaColor,outline="black")
+    canvas.update()
+        
+
+win.mainloop()
---
+++ teidefigyelj.py
@@ -0,0 +1,11 @@
+import random
+
+
+#12 fő, 8-12, jegy:1-5
+jegyek=[]
+for i in range(12):
+    for k in range(random.randrange(8,13)):
+        jegy=random.randrange(1,6)
+        egyDiak.append(jegy)
+
+    jegyek.append(egyDiak)
